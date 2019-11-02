eb6fedb60c8b2418a8f5f8780e655344083b07ef
John L. Ries <jries@salford-systems.com>
2019-11-02 15:27:52 -0700

    Amendment to rundemos.sh
    Add invocation of --baseout to the job creating bostn2a.cmd.
    This creates a separate command file for each value of LOSSFUNC.

--- demos/rundemos.sh
+++ demos/rundemos.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 SCMDGEN=../scmdgen
 cat scmdgen_demo.txt | $SCMDGEN N=1:10 >scmdgen_demo.cmd
-$SCMDGEN --input=bostn2a.txt --output=bostn2a.cmd LOSSFUNC=LAD,LS,HUBER,RF
+$SCMDGEN --input=bostn2a.txt --output=bostn2a.cmd --baseout=bostn2a \
+         --use_values LOSSFUNC=LAD,LS,HUBER,RF
 $SCMDGEN --input=2digit.txt --output=2digit.cmd N=0:9 M=0:9
 $SCMDGEN --input=2digit.txt --output=2digitoe.cmd N=0:8:2 M=1:9:2
