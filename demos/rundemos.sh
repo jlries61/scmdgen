b96b7d71fae93066b843d05ed0816747ebac9df3
John L. Ries <jries@salford-systems.com>
2019-11-02 14:43:37 -0700

    Fix typo in rundemos.sh

--- demos/rundemos.sh
+++ demos/rundemos.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-$SCMDGEN=../scmdgen
+SCMDGEN=../scmdgen
 cat scmdgen_demo.txt | $SCMDGEN N=1:10 >scmdgen_demo.cmd
 $SCMDGEN --input=bostn2a.txt --output=bostn2a.cmd LOSSFUNC=LAD,LS,HUBER,RF
 $SCMDGEN --input=2digit.txt --output=2digit.cmd N=0:9 M=0:9
